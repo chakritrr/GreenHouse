e74d5a40d92b85bf58b83e61b144d5129262ed44
chakritrr <127234504+chakritrr@users.noreply.github.com>
2024-11-01 10:01:05 +0700

    BE: Add Swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+  
   await app.listen(3000);
 }
 bootstrap();
